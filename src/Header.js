05557232a4b2bfa96d1830b3746a9e6ac1cb078f
Paddy McGlone <paddymcglone@gmail.com>
2021-04-06 22:31:24 +0100

    Adding the header icons.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./Header.css";
+import logo from './images/tinder-logo.png';
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from "@material-ui/icons/Forum";
+import Forum from '@material-ui/icons/Forum';
 
 function Header() {
     return (
-        <div className="header">
-            <h2>I am the header</h2>
+        <div className="header">            
+            <IconButton>
+                <PersonIcon fontSize="Large" className="header_icon"/>
+            </IconButton>            
+
+            <img 
+                className="header__logo"
+                src={logo}
+                alt="tinder logo"
+            />
+
+            <ForumIcon />
+
         </div>
     )
 }
