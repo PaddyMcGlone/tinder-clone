0b11faf0930465b19446624c8f9e43bae5d6a58c
Paddy McGlone <paddymcglone@gmail.com>
2021-04-08 12:24:29 +0100

    Adding prettier changes.

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -5,48 +5,50 @@ import "./TinderCards.css";
 // Imported Tinder-Card React package for managing the cards
 
 function TinderCards() {
-const [people, setPeople] = useState([
+  const [people, setPeople] = useState([
     {
-        name: "Elon Musk",
-        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg/220px-Elon_Musk_Royal_Society_%28crop1%29.jpg",
+      name: "Elon Musk",
+      url:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg/220px-Elon_Musk_Royal_Society_%28crop1%29.jpg",
     },
     {
-        name: "Jeff Bezos",
-        url: "https://variety.com/wp-content/uploads/2019/12/jeff_bezos2019_rev-1.png",
+      name: "Jeff Bezos",
+      url:
+        "https://variety.com/wp-content/uploads/2019/12/jeff_bezos2019_rev-1.png",
     },
-]);
+  ]);
 
-const swiped = (direction, nameToDelete) => {
+  const swiped = (direction, nameToDelete) => {
     console.log("Removing: " + nameToDelete);
     //setLastDirection(direction);
-};
+  };
 
-const outOfFrame = (name) => {
+  const outOfFrame = (name) => {
     console.log(name + "left of the screen");
-};
+  };
 
-    return (
-        <div className="tinderCards">
-            <div className="tinderCards__cardContainer">
-                {people.map((person) => (
-                    <TinderCard
-                        className="swipe"
-                        key={person.name}
-                        preventSwipe={["up","down"]}
-                        onSwipe={(dir) => swiped(dir, person.name)}
-                        onCardLeftScreen={() => outOfFrame(person.name)} 
-                    >
-                        <div
-                         style={{ backgroundImage: `url(${person.url})` }}
-                         className="card"
-                         >
-                            <h3>{person.name}</h3>
-                        </div>
-                    </TinderCard>
-                ))}
-            </div>            
-        </div>
-    )
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default TinderCards
+export default TinderCards;
